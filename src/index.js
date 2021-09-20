22be09e89f2cb3cfd93985fee638aaa2919393f3
devikamehr <devikamehr@gmail.com>
2021-09-20 14:32:07 -0700

    Finished class work.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const data = [
+    {
+        id: 512,
+        name: "Neil Rhodes",
+        email: "rhodes@hmc.edu",
+        phone: "(909) 555-1212"
+    },
+    {
+        id: 787,
+        name: "Barack Obama",
+        email: "ex-prez@whitehouse.gov",
+        phone: "(312) 555-1212"
+    }
+
+];
+const dataJsx = data.map((info) => {
+    return <div className="tagline" key={info.id}>
+        <p id="name"> {info.name} </p>
+        <p> {info.email}</p>
+        <p> {info.phone} </p>
+    </div>
+})
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+      <strong><h1> People </h1></strong>
+      {dataJsx}
   </React.StrictMode>,
   document.getElementById('root')
 );
